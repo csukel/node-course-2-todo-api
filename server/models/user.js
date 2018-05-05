b7f3a44c133a56f1bb83cbff62cbe06b380be211
Loucas Stylianou <l.stylianou@ams.lan>
2018-05-05 10:01:59 +0300

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,66 @@
-var {mongoose} = require('../db/mongoose')
+const {mongoose} = require('../db/mongoose')
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema( {
     email: {
         required: true,
         type: String,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+        
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
+UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id','email']);
+};
+
+//instance methods
+//we need to bind this (context operator) hence we use a function instead of arrow function
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(),access},'abc123').toString();
+
+    // user.tokens.push({access,token});
+    user.tokens = user.tokens.concat([{access,token}]);
+    return user.save()
+        .then(()=>{
+            return token;
+        },(e)=>{
+            return e;
+        })
+        .catch((e)=>{
+            return e;
+        });
+
+};
+
+var User = mongoose.model('User', UserSchema );
+
 module.exports = {User};
-
-// var newUser = new User({
-//     email: 'l.stylianou@ams.com.cy'
-// });
-
-// newUser.save()
-//     .then((doc)=>{
-//         console.log(JSON.stringify(doc,undefined,2));
-//     })
-//     .catch((err)=>{
-//         console.log('Unable to save user',err);
-//     })
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} =require('mongodb');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -31,72 +31,101 @@ app.post('/todos', (req, res) => {
 });
 
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos});
-    }).catch((e)=>res.status(400).send(e))
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }).catch((e) => res.status(400).send(e))
 });
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
-    
-    if (!ObjectID.isValid(id)){
-        return res.status(404).send({status:'Error',message:"Not a valid document id"})
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ status: 'Error', message: "Not a valid document id" })
     }
     Todo.findById(req.params.id)
-        .then((todo)=>{
-            if (!todo){
-                return res.status(404).send({status:'Warning',message:'No match found'})
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send({ status: 'Warning', message: 'No match found' })
             }
-            res.status(200).send({status:'OK',todo});
-        }).catch((err)=>{
+            res.status(200).send({ status: 'OK', todo });
+        }).catch((err) => {
             res.status(400).send();
         })
 });
 
 
-app.delete('/todos/:id', (req,res)=>{
+app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
 
-    if (!ObjectID.isValid(id)){
-        return res.status(404).send({status:'Error',message:"Not a valid document id"})
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ status: 'Error', message: "Not a valid document id" })
     }
     Todo.findByIdAndRemove(req.params.id)
-        .then((todo)=>{
-            if (!todo){
-                return res.status(404).send({status:'Warning',message:'No document found to remove'})
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send({ status: 'Warning', message: 'No document found to remove' })
             }
-            res.status(200).send({status:'OK',todo});
-        }).catch((err)=>{
+            res.status(200).send({ status: 'OK', todo });
+        }).catch((err) => {
             res.status(400).send();
         });
 });
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     //pick the properties that are allowed to be updated
-    var body = _.pick(req.body,['text','completed']);
+    var body = _.pick(req.body, ['text', 'completed']);
 
-    if (!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    if (_.isBoolean(body.completed) && body.completed){
+    if (_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
-    }else {
+    } else {
         body.completed = false;
         body.completedAt = null;
     }
-    Todo.findByIdAndUpdate(id,{$set: body},{new: true})
-        .then((todo)=>{
-            if (!todo){
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then((todo) => {
+            if (!todo) {
                 return res.status(404).send();
             }
-            res.send({todo});
+            res.send({ todo });
         })
-        .catch((err)=>{
+        .catch((err) => {
             res.status(400).send();
         })
 })
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    //model method
+    //User.findByToken
+
+    //instance method
+    //user.generateAuthToken
+
+    user.save()
+        .then(() => {
+            // res.send(user);
+            user.generateAuthToken()
+                .then((token) => {
+                    res.header('x-auth', token).send(user);
+                }, (e) => {
+                    res.status(400).send(e);
+                })
+        }, (e) => {
+            res.status(400).send(e);
+        })
+
+        .catch((err) => {
+            res.status(400).send(err);
+        })
+});
 
 app.listen(port, () => {
     console.log(`Started on port: ${port}`);
@@ -104,4 +133,4 @@ app.listen(port, () => {
 
 
 
-module.exports = {app};
+module.exports = { app };
